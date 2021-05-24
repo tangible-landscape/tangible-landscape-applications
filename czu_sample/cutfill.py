348af011817272f9dc500873570ac76069fc3df4
Anna Petrasova <kratochanna@gmail.com>
2021-05-24 16:23:04 -0400

    small changes to config and cut and fill

--- czu_sample/cutfill.py
+++ czu_sample/cutfill.py
@@ -4,21 +4,27 @@ from tangible_utils import get_environment
 
 
 def run_cutfill(real_elev, scanned_elev, zexag, env, **kwargs):
-    # compute difference and set color table using standard deviations
-    gs.mapcalc("diff = {r} - {s}".format(r=real_elev, s=scanned_elev), env=env)
-    gs.mapcalc("absdiff = abs(diff)", env=env)
+    tmp = 'tmp_resampled'
+    new = 'diff'
+    gs.run_command('r.resamp.interp', input=real_elev, output=tmp,
+                   method='bilinear', env=env)
+    gs.mapcalc(f"{new} = {tmp} - {scanned_elev}", env=env)
     univar = gs.parse_command("r.univar", flags="g", map=real_elev, env=env)
     std1 = zexag * float(univar["stddev"])
     std2 = zexag * 2 * std1
-    maxv = zexag * float(univar["max"]) + 1
-    minv = zexag * float(univar["min"]) - 1
+    std3 = zexag * 3 * std1
     rules = [
-        f"-{maxv} black",
+        f"-1000000 black",
+        f"-{std3} black",
         f"-{std2} 202:000:032",
         f"-{std1} 244:165:130",
         "0 247:247:247",
         f"{std1} 146:197:222",
         f"{std2} 5:113:176",
-        f"{minv} black",
+        f"{std3} black",
+        f"1000000 black",
     ]
-    gs.write_command("r.colors", map="diff", rules="-", stdin="\n".join(rules), env=env)
+    gs.write_command("r.colors", map=new, rules="-", stdin="\n".join(rules), env=env)
+    # analyses.difference(real_elev=real_elev, scanned_elev=scanned_elev,
+    #                     new='diff', zexag=zexag, env=env)
+
